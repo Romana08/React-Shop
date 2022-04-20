6542b8cfcab19fcafd6553cc8517cf6a37375a26
Romana08 <mortonromina@gmail.com>
2022-04-19 22:21:16 -0300

    Componentes separados

---
+++ src/pages/MyAccount.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+const Account = ()=>{
+    return(
+        <div>
+            <h3>
+                queueuejeje
+            </h3>
+        </div>
+    )
+}
+
+export default Account;
---
+++ src/pages/Orders.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+const Orders = ()=>{
+    return(
+        <div>
+            <h3>
+                Ordenes
+            </h3>
+        </div>
+    )
+}
+
+export default Orders;
